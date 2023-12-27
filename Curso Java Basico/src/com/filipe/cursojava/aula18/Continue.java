6aca37f2f67327af58fdb0480561a509bca550c1
FilipeAPorto <filipealvesmissias@hotmail.com>
2023-12-27 16:27:56 -0300

    Aula 18 - Conteudo sobre Continue.

---
+++ Curso Java Basico/src/com/filipe/cursojava/aula18/Continue.java
@@ -0,0 +1,24 @@
+package com.filipe.cursojava.aula18;
+
+import java.util.Scanner;
+
+public class Continue {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entre com um número :");
+        int num = scan.nextInt();
+
+        System.out.print("Entre com um limite :");
+        int max = scan.nextInt();
+
+        for (int i=num;i<=max;i++){
+
+            if (i % 7 == 0){
+                continue;
+            }
+            System.out.println("O valor de i é "+i);
+        }
+    }
+}
