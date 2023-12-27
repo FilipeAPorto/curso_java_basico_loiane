487c965f72da95a77f5b05ab11e0944a49ed2e06
FilipeAPorto <filipealvesmissias@hotmail.com>
2023-12-27 16:27:38 -0300

    Aula 18 - Conteudo sobre Break.

---
+++ Curso Java Basico/src/com/filipe/cursojava/aula18/Break.java
@@ -0,0 +1,25 @@
+package com.filipe.cursojava.aula18;
+
+import java.util.Scanner;
+
+public class Break {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entre com um número :");
+        int num = scan.nextInt();
+
+        System.out.print("Entre com um limite :");
+        int max = scan.nextInt();
+
+
+        for (int i=num;i<=max;i++){
+
+            if (i % 7 == 0){
+                System.out.println("O valor de i é "+i);
+                break;
+            }
+        }
+    }
+}
