115263caf6fa684d67adcc7a6962d8647a26e4a7
FilipeAPorto <filipealvesmissias@hotmail.com>
2023-12-28 18:23:25 -0300

    Exercicio 17 - Concluído

---
+++ Atividades/src/aula19/exercicios/Ex17.java
@@ -0,0 +1,35 @@
+package aula19.exercicios;
+
+/*Ler um vetor A com 10 elementos inteiros correspondentes as idades
+de um grupo de pessoas. Escreva um programa que determine e
+escreva a quantidade de pessoas que possuem idade superior a 35 anos.*/
+public class Ex17 {
+    public static void main(String[] args) {
+
+        int pessoa = 0;
+
+        int[] vetorA = new int[10];
+        vetorA[0] = 40;
+        vetorA[1] = 29;
+        vetorA[2] = 15;
+        vetorA[3] = 76;
+        vetorA[4] = 5;
+        vetorA[5] = 8;
+        vetorA[6] = 3;
+        vetorA[7] = 27;
+        vetorA[8] = 15;
+        vetorA[9] = 87;
+
+
+        for (int i=0;i< vetorA.length;i++){
+
+            if (vetorA[i] >= 35){
+
+                pessoa++;
+            }
+        }
+
+        System.out.println("\nA quantidade de pessoas que possuem idade superior a 35 anos são " + pessoa);
+
+    }
+}
