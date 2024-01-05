43fbad1438dd23dc1b85a6a1707098887df0281e
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-05 13:27:31 -0300

    Aula 20 - Exercicio 3 - Concluido - Atualizado

--- Atividades/src/aula20/exercicios/Ex3.java
+++ Atividades/src/aula20/exercicios/Ex3.java
@@ -3,22 +3,22 @@ package aula20.exercicios;
 /*Capture do teclado valores para preenchimento de uma matriz M 3x3.
 Após a captura imprima a matriz criada e encontre a quantidade de números pares, a quantidade de números ímpares.*/
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class Ex3 {
     public static void main(String[] args) {
 
         int[][] matrizM = new int[3][3];
 
-        //teste
-        Random gerador = new Random();
-
+        Scanner scan = new Scanner(System.in);
 
         for (int i = 0;i < matrizM.length;i++){
             for(int j = 0;j < matrizM[i].length;j++){
 
-                matrizM[i][j] = gerador.nextInt();
+                System.out.print("Insira o "+(j+1)+"º numero da "+(i+1)+"ª coluna :");
+                matrizM[i][j] = scan.nextInt();
             }
+            System.out.println();
         }
 
         int par=0,impar=0;
@@ -27,15 +27,11 @@ public class Ex3 {
             for(int j = 0;j < matrizM[i].length;j++) {
 
                 if (matrizM[i][j] % 2 == 0){
-
                     par++;
                 } else {
-
                     impar++;
                 }
-                System.out.println(matrizM[i][j]);
             }
-            System.out.println();
         }
         System.out.println("Numeros Pares - "+par +" \nNumeros Impares - " + impar);
     }
