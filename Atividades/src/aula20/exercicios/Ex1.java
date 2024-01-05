3d3baa8d7b8f9e0dc5cfd32c3c32f6d8e69d2a96
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-05 12:43:57 -0300

    Aula 20 - Exercicio 1 - Concluido - Atualizado

--- Atividades/src/aula20/exercicios/Ex1.java
+++ Atividades/src/aula20/exercicios/Ex1.java
@@ -1,28 +1,24 @@
 package aula20.exercicios;
 
-import java.util.Scanner;
+import java.util.Random;
 
 /*Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0-9.
 Após isso determine o maior número da matriz e a sua posição (linha, coluna).*/
 public class Ex1 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Random gerador = new Random();
 
         int[][] matrizM = new int[4][4];
 
-        System.out.println("Vamos criar uma matriz 4x4.");
+        System.out.println("Gerando uma matriz 4x4.");
 
         for(int i = 0;i < matrizM.length;i++){
             for(int j = 0;j < matrizM[i].length;j++){
 
-                do {
-                    System.out.print("Insira um valor de 0 a 9 para a " + (i + 1) + "ª linha e " + (j + 1) + "ª coluna :");
-                    matrizM[i][j] = scan.nextInt();
-                    if (matrizM[i][j] < 0 || matrizM[i][j]>9){
-                        System.out.println("*** ERRO - INSIRA UM VALOR VÁLIDO ***\n");
-                    }
-                }while (matrizM[i][j] < 0 || matrizM[i][j]>9);
+                matrizM[i][j] = gerador.nextInt(100);
+
+                System.out.print(matrizM[i][j]+ " ");
             }
             System.out.println();
         }
@@ -39,6 +35,8 @@ public class Ex1 {
                 }
             }
         }
-        System.out.println("O maior valor da matriz é "+maiorNum+" e esta na posição "+(linha+1)+" - "+(coluna+1));
+        System.out.println("\nO maior valor da matriz é "+maiorNum);
+        System.out.println("Esta na linha " +(linha+1));
+        System.out.println("Esta na coluna "+(coluna+1));
     }
 }
