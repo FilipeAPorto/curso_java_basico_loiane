55ce833766536a38206c50ce73abff38b9a725a2
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-05 13:19:19 -0300

    Aula 20 - Exercicio 2 - Concluido - Atualizado

--- Atividades/src/aula20/exercicios/Ex2.java
+++ Atividades/src/aula20/exercicios/Ex2.java
@@ -1,69 +1,61 @@
 package aula20.exercicios;
 
-import java.util.Scanner;
+import java.util.Random;
 
 /*Gere e imprima uma matriz M 10x10 com valores aleatórios entre 0-9.
 Após isso indique qual é o maior e o menor valor da linha 5 e qual é o maior e o menor valor da coluna 7.*/
 public class Ex2 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Random gerador = new Random();
 
         int[][] matrizM = new int[10][10];
 
-        System.out.println("Vamos criar uma matriz 10x10.");
+        System.out.println("\n**** Vamos criar uma matriz 10x10 Entre 10 e 99 ****\n");
 
         for(int i = 0;i < matrizM.length;i++){
-
-            System.out.println("Insira um valor de 0 a 9.");
-
             for(int j = 0;j < matrizM[i].length;j++){
 
-                do {
-                    System.out.print("Para a " + (i + 1)+ "ª linha "+ (j + 1) + "ª coluna :");
-                    matrizM[i][j] = scan.nextInt();
-                    if (matrizM[i][j] < 0 || matrizM[i][j]>9){
-                        System.out.println("*** ERRO - INSIRA UM VALOR VÁLIDO ***\n");
-                    }
-                }while (matrizM[i][j] < 0 || matrizM[i][j]>9);
+               matrizM[i][j] = gerador.nextInt(10,99);
+
+                System.out.print(matrizM[i][j] + " | ");
             }
-           System.out.println();
+            System.out.println();
         }
 
         //Codigo para verificar o maior e menor valor da linha 5.
 
         int linha5 = 4,coluna7 = 6;
-        int maiorNum = Integer.MIN_VALUE;
-        int menorNum = Integer.MAX_VALUE;
+        int maiorNumL5 = Integer.MIN_VALUE;
+        int menorNumL5 = Integer.MAX_VALUE;
 
             for (int j=0;j < matrizM[linha5].length;j++){
 
-                if (matrizM[linha5][j] > maiorNum){
-                    maiorNum = matrizM[linha5][j];
+                if (matrizM[linha5][j] > maiorNumL5){
+                    maiorNumL5 = matrizM[linha5][j];
                 }
 
-                if (matrizM[linha5][j] < menorNum){
-                    menorNum = matrizM[linha5][j];
+                if (matrizM[linha5][j] < menorNumL5){
+                    menorNumL5 = matrizM[linha5][j];
                 }
             }
-        System.out.println("Na linha "+(linha5+1) + " o maior valor inserido é " + maiorNum + " e o menor valor inserido é " + menorNum);
-
+        System.out.println("\nNa linha "+(linha5+1) + " o maior valor inserido é " + maiorNumL5 + " e o menor valor inserido é " + menorNumL5);
 
             //Codigo para verificar o maior e o menor valor da coluna 7.
 
-        maiorNum = Integer.MIN_VALUE;
-        menorNum = Integer.MAX_VALUE;
+        int maiorNumC7 = Integer.MIN_VALUE;
+        int menorNumC7 = Integer.MAX_VALUE;
 
-        for (int i=0;i < matrizM[linha5].length;i++){
+        for (int i=0;i < matrizM.length;i++){
 
-            if (matrizM[i][coluna7] > maiorNum){
-                maiorNum = matrizM[i][coluna7];
+            if (matrizM[i][coluna7] > maiorNumC7){
+                maiorNumC7 = matrizM[i][coluna7];
             }
 
-            if (matrizM[i][coluna7] < menorNum){
-                menorNum = matrizM[i][coluna7];
+            if (matrizM[i][coluna7] < menorNumC7){
+                menorNumC7 = matrizM[i][coluna7];
             }
         }
-        System.out.println("Na linha "+(coluna7+1) + " o maior valor inserido é " + maiorNum + " e o menor valor inserido é " + menorNum);
+        System.out.println("Na coluna "+(coluna7+1) + " o maior valor inserido é " + maiorNumC7 + " e o menor valor inserido é " + menorNumC7);
     }
 }
