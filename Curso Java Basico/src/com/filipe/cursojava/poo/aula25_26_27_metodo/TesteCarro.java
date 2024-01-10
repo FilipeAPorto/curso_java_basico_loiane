30dce45b0ff3e0e1fcb90c8dfca5ace2fc4313de
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-09 21:43:39 -0300

    Aula 25,26 e 27 - Introdução à metodos - atualização de informações no código

--- Curso Java Basico/src/com/filipe/cursojava/poo/aula25_26_27_metodo/TesteCarro.java
+++ Curso Java Basico/src/com/filipe/cursojava/poo/aula25_26_27_metodo/TesteCarro.java
@@ -13,18 +13,27 @@ public class TesteCarro {
         System.out.println(van.marca);
         System.out.println(van.modelo);
 
+        //Metodos sem retorno
+        System.out.println("*** Método sem retorno ***");
         van.exibirAutonomia();
+        System.out.println();
 
+        //Metodo com retorno
+        System.out.println("*** Método com  retorno ***");
         double autonomia = van.obterAutonomia();
         System.out.println("Autonomia do carro é "+autonomia);
 
         System.out.println("Autonomia do carro é "+van.obterAutonomia());
+        System.out.println();
 
+        //Metodo com parâmetro
+        System.out.println("*** Método com parâmetro ***");
         double qtdCombustivel10 = van.calcularCombustivel(10);
         double qtdCombustivel15 = van.calcularCombustivel(15);
 
         System.out.println("qtdCombustivel10 " + qtdCombustivel10);
         System.out.println("qtdCombustivel15 " + qtdCombustivel15);
+
 
     }
 }
