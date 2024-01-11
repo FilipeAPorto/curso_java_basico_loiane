2048c40c88e41c7dffa023a89a0e4d7e8a51da06
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-11 18:42:28 -0300

    Aula 33 - Sobre sobrecarga de metodos e construtores

---
+++ Curso Java Basico/src/com/filipe/cursojava/poo/aula33_sobrecarga/TesteCalculadora.java
@@ -0,0 +1,11 @@
+package com.filipe.cursojava.poo.aula33_sobrecarga;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+
+        MinhaCalculadora calc = new MinhaCalculadora();
+
+        calc.somar(1,2);
+        calc.somar(1.2,1.9);
+    }
+}
