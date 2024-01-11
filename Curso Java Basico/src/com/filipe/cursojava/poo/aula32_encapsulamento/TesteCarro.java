08b5780579f7c96fd0efe0b8d41520e218fe414d
FilipeAPorto <filipealvesmissias@hotmail.com>
2024-01-11 18:26:10 -0300

    Aula 32 - Sobre encapsulamento.

---
+++ Curso Java Basico/src/com/filipe/cursojava/poo/aula32_encapsulamento/TesteCarro.java
@@ -0,0 +1,14 @@
+package com.filipe.cursojava.poo.aula32_encapsulamento;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+
+
+        Carro van = new Carro();
+
+        van.setMarca("Fiat");
+
+        System.out.println(van.getMarca());
+
+    }
+}
